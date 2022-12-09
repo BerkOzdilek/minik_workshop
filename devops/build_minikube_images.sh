13111064ecf13e736efd4c5d25cb6ba982adbd76
Berk Ozdilek <biozdilek@gmail.com>
2022-12-09 14:50:32 +0100

    Update ingress v1 from beta

--- devops/build_minikube_images.sh
+++ devops/build_minikube_images.sh
@@ -2,7 +2,7 @@
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 TAG_BACKEND=minik_backend:v1
-TAG_WEB_APP=minik_web_app:v2
+TAG_WEB_APP=minik_web_app:v1
 
 # use minikube's docker
 eval $(minikube docker-env)
